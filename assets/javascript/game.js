059c78e620ed1c226254f846c79e0916d6dbf9f1
Leah Dodson <leahdodson9@gmail.com>
2020-01-03 23:53:12 -0600

    added reset win, loss functions, added spanid for user guess and changed style elements for user guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-	//!Define global variables
+	//!define global variables
 	//variable for the target number
 	var targetNum = Math.floor(Math.random() * 101 + 19);
-	console.log("targetNum: ", targetNum);
+	console.log("starting target number is ", targetNum);
 	//add targetNum to html
 	$("#number").text(targetNum);
 
@@ -22,4 +22,55 @@ $(document).ready(function() {
 	var losses = 0;
 	//add losses to html
 	$("#total-losses").text(losses);
+
+	//!define functions
+	//reset function
+	function reset() {
+		//reset userNum and show it in html as grey
+		userNum = 0;
+		$("#user-total").text(userNum);
+		$("#user-total").css("color", "lightgray");
+		//choose a new targetNum and display in html
+		targetNum = Math.floor(Math.random() * 101 + 19);
+		console.log("target number is now ", targetNum);
+		$("#number").text(targetNum);
+		//choose new random gem values
+		num1 = Math.floor(Math.random() * 11 + 1);
+		num2 = Math.floor(Math.random() * 11 + 1);
+		num3 = Math.floor(Math.random() * 11 + 1);
+		num4 = Math.floor(Math.random() * 11 + 1);
+	}
+	//win function
+	function win() {
+		//increment wins
+		wins++;
+		//tell user they win
+		alert("Congratulations, you win!");
+		//display in html
+		$("#total-wins").text(wins);
+		//run the reset
+		reset();
+	}
+	//loss function
+	function loss() {
+		//increment losses
+		losses++;
+		//tell user they lost
+		alert("Sorry, you lost. Try again!");
+		//display in html
+		$("#total-losses");
+	}
+
+	//gem clicks
+	$("gem-1").on("click", function() {
+		userNum = userNum + num1;
+		console.log("Current user number: " + userNum);
+		//update html with current userNum
+		$("#user-number").text(userNum);
+		//check win condition
+		if (userNum == targetNum) {
+			win();
+		} else if (userNum > targetNum) {
+		}
+	});
 });
